6ce62862427ca07c9ea067566415e4349a83fd04
刘祎晗 <2524967263@qq.com>
2024-11-07 13:10:24 +0800

    个人项目内容更新

--- my-blog/components/card.tsx
+++ my-blog/components/card.tsx
@@ -17,14 +17,14 @@ export default function Card ({title, description, imgSrc, github,slug}:CardProp
                             <Image alt={title} src={imgSrc} className="object-cover object-center md:h-36 lg:h-48" width={544} height={306}></Image>
                         )
                     }
-                </div>
-                <div className="p-6">
-                    <h2 className="mb-3 text-2xl font-bold leading-8 tracking-tight">
-                        {title}
-                    </h2>
-                    <p className="prose mb-3 max-w-none text-gray-500 dark:text-gray-400">
-                        {description}
-                    </p>
+                    <div className="p-6">
+                        <h2 className="mb-3 text-2xl font-bold leading-8 tracking-tight">
+                            {title}
+                        </h2>
+                        <p className="prose mb-3 max-w-none text-gray-500 dark:text-gray-400">
+                            {description}
+                        </p>
+                    </div>
                 </div>
             </Link>
         </div>
